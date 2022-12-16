8bb5b221291319141750faaaf2406de4d9eccaf5
Anri <anri0806@hotmail.co.jp>
2022-12-16 16:04:39 -0500

    added icons to links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+import githubLogo from "../images/github-mark.png";
+import linkedInLogo from "../images/LI-In-Bug.png";
+import mediumLogo from "../images/medium.png";
+
+
 function Home() {
   return (
     <>
       <h1>Hi, I'm Anri. I'm a software engineer.</h1>
+      <img src={githubLogo} alt="github logo" />
+      <img src={linkedInLogo} alt="linkedIn logo" />
+      <img src={mediumLogo} alt="medium logo" />
     </>
   );
 }
