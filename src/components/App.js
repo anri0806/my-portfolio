b2ce20ee14904dad7c53ea5f52747319fa4a472f
Anri <anri0806@hotmail.co.jp>
2022-12-16 15:45:49 -0500

    added home component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Home from "./Home";
+
+import "../index.css";
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Hi, I'm Anri. I'm a software engineer.</h1>
+    </>
+  );
+}
+
+export default Home;
