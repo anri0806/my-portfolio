cd60e4e8ead077a679d95b15ab3fa9de2c603319
Anri <anri0806@hotmail.co.jp>
2022-12-16 16:08:45 -0500

    added links

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,19 @@ import githubLogo from "../images/github-mark.png";
 import linkedInLogo from "../images/LI-In-Bug.png";
 import mediumLogo from "../images/medium.png";
 
-
 function Home() {
   return (
     <>
       <h1>Hi, I'm Anri. I'm a software engineer.</h1>
-      <img src={githubLogo} alt="github logo" />
-      <img src={linkedInLogo} alt="linkedIn logo" />
-      <img src={mediumLogo} alt="medium logo" />
+      <a href="https://github.com/anri0806">
+        <img src={githubLogo} alt="github logo" />
+      </a>
+      <a href="https://www.linkedin.com/in/anri-kawahara-63460358/">
+        <img src={linkedInLogo} alt="linkedIn logo" />
+      </a>
+      <a href="https://medium.com/@anri.kawahara">
+        <img src={mediumLogo} alt="medium logo" />{" "}
+      </a>
     </>
   );
 }
