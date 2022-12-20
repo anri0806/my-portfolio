4aeb3822c2e4fa80ce7eaf72037e7dd86725994e
Anri <anri0806@hotmail.co.jp>
2022-12-19 22:54:36 -0500

    added logos to skill page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,13 @@
-import htmlLogo from "../images/HTML5_Logo_256.png";
+import htmlLogo from "../images/html.png";
 import cssLogo from "../images/css.png";
+import jsLogo from "../images/js.png";
+import reactLogo from "../images/react.png";
+import rubyLogo from "../images/ruby.png";
+import githubLogo from "../images/github-mark.png";
+import railsLogo from "../images/rails.png";
+import nodeLogo from "../images/nodeJS.png";
+import postgresLogo from "../images/PostgreSQL.png";
+import bootstrapLogo from "../images/bootstrap.png";
 
 function Skills() {
   return (
@@ -7,6 +15,14 @@ function Skills() {
       <h1>Skills</h1>
       <img className="skills-logo" src={htmlLogo} alt="html" />
       <img className="skills-logo" src={cssLogo} alt="css" />
+      <img className="skills-logo" src={jsLogo} alt="js" />
+      <img className="skills-logo" src={reactLogo} alt="react" />
+      <img className="skills-logo" src={rubyLogo} alt="ruby" />
+      <img className="skills-logo" src={railsLogo} alt="rails" />
+      <img className="skills-logo" src={githubLogo} alt="github" />
+      <img className="skills-logo" src={nodeLogo} alt="nodeJS" />
+      <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" />
+      <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" />
     </>
   );
 }
