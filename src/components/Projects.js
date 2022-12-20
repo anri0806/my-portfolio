5394a0ef68ed1f14b5922dbd4f2735b8d4aa52da
Anri <anri0806@hotmail.co.jp>
2022-12-19 23:08:19 -0500

    added projects content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,37 @@
-
 function Projects() {
-    return(
-        <>
-        <h1>Projects</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Projects</h1>
+      <h4>Invite Only</h4>
+      <a href="https://youtu.be/GQoX64CcZJA">
+        <p>demo</p>
+      </a>
+      <a href="https://github.com/anri0806/invite-only">
+        <p>code</p>
+      </a>
+      <h4>Family Album</h4>
+      <a href="https://youtu.be/2WOkl2WyfqE">
+        <p>demo</p>
+      </a>
+      <a href="https://github.com/anri0806/family-album">
+        <p>code</p>
+      </a>
+      <h4>Baby Logbook</h4>
+      <a href="https://youtu.be/NNqnDEDgGkM">
+        <p>demo</p>
+      </a>
+      <a href="https://github.com/anri0806/baby_logbook_frontend">
+        <p>code</p>
+      </a>
+      <h4>Flags of the World</h4>
+      <a href="">
+        <p>demo</p>
+      </a>
+      <a href="https://github.com/anri0806/flags-of-the-world">
+        <p>code</p>
+      </a>
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
