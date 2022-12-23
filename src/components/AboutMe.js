47ae0d01cce504800870774f05b2041dad784c3a
Anri <anri0806@hotmail.co.jp>
2022-12-23 15:28:50 -0500

    modified home background

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,6 +5,9 @@ import dogLogo from "../images/boston-terrier.png";
 import exerciseLogo from "../images/exercises.png";
 import carpenterLogo from "../images/carpenter.png";
 import pianoLogo from "../images/piano.png";
+import githubLogo from "../images/github-mark.png";
+import linkedInLogo from "../images/LI-In-Bug.png";
+import mediumLogo from "../images/medium.png";
 
 function AboutMe() {
   return (
@@ -29,15 +32,29 @@ function AboutMe() {
         I believe that the passion can be driving force to learn more and give
         me all important sense of purpose to be the best possible version of me.
       </p>
+      <div className="title">
+        <a href="https://github.com/anri0806">
+          <img className="logo-link" src={githubLogo} alt="github logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/anri-kawahara-63460358/">
+          <img className="logo-link" src={linkedInLogo} alt="linkedIn logo" />
+        </a>
+        <a href="https://medium.com/@anri.kawahara">
+          <img className="logo-link" src={mediumLogo} alt="medium logo" />{" "}
+        </a>
+      </div>
       <p>Aside from work, my passions are:</p>
       <img className="about-me-logo" src={travelLogo} alt="traveling" />
       <img className="about-me-logo" src={hikingLogo} alt="hiking" />
       <img className="about-me-logo" src={gameLogo} alt="game" />
       <img className="about-me-logo" src={dogLogo} alt="boston terrier" />
       <img className="about-me-logo" src={exerciseLogo} alt="exercise" />
-      <img className="about-me-logo" src={carpenterLogo} alt="build and make things" />
+      <img
+        className="about-me-logo"
+        src={carpenterLogo}
+        alt="build and make things"
+      />
       <img className="about-me-logo" src={pianoLogo} alt="playing piano" />
-
 
       <br />
       <a href="https://www.flaticon.com" title="icons">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,8 @@
-import githubLogo from "../images/github-mark.png";
-import linkedInLogo from "../images/LI-In-Bug.png";
-import mediumLogo from "../images/medium.png";
 
 function Home() {
   return (
     <div className="home">
-      <div className="title">
-        <h1>Hello, I'm Anri Kawahara.</h1>
-        <a href="https://github.com/anri0806">
-          <img className="logo-link" src={githubLogo} alt="github logo" />
-        </a>
-        <a href="https://www.linkedin.com/in/anri-kawahara-63460358/">
-          <img className="logo-link" src={linkedInLogo} alt="linkedIn logo" />
-        </a>
-        <a href="https://medium.com/@anri.kawahara">
-          <img className="logo-link" src={mediumLogo} alt="medium logo" />{" "}
-        </a>
-      </div>
+
     </div>
   );
 }
