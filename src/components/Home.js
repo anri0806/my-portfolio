126ac893243abf933a3e4a2bad0f19dae8588d4c
Anri <anri0806@hotmail.co.jp>
2022-12-22 21:12:44 -0800

    modified title

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,19 @@ import mediumLogo from "../images/medium.png";
 
 function Home() {
   return (
-    <div>
-      <h1>Hi, I'm Anri. I'm a software engineer.</h1>
-      <a href="https://github.com/anri0806">
-        <img className="logo-link" src={githubLogo} alt="github logo" />
-      </a>
-      <a href="https://www.linkedin.com/in/anri-kawahara-63460358/">
-        <img className="logo-link" src={linkedInLogo} alt="linkedIn logo" />
-      </a>
-      <a href="https://medium.com/@anri.kawahara">
-        <img className="logo-link" src={mediumLogo} alt="medium logo" />{" "}
-      </a>
+    <div className="home">
+      <div className="title">
+        <h1>Hello, I'm Anri Kawahara.</h1>
+        <a href="https://github.com/anri0806">
+          <img className="logo-link" src={githubLogo} alt="github logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/anri-kawahara-63460358/">
+          <img className="logo-link" src={linkedInLogo} alt="linkedIn logo" />
+        </a>
+        <a href="https://medium.com/@anri.kawahara">
+          <img className="logo-link" src={mediumLogo} alt="medium logo" />{" "}
+        </a>
+      </div>
     </div>
   );
 }
