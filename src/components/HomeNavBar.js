776f64055c886e6f4172c328353b7d62a8222a2a
Anri <anri0806@hotmail.co.jp>
2023-01-01 23:38:49 -0500

    added href to nav link

--- src/components/HomeNavBar.js
+++ src/components/HomeNavBar.js
@@ -5,18 +5,17 @@ import skillsButton from "../images/2.png";
 import projectsButton from "../images/3.png";
 
 function HomeNavBar() {
-
   // style={{ margin: "0", padding: "0", width: "30%" }}
 
   return (
     <Nav className="nav-bar">
-      <Nav.Link to="about" className="home-nav-link">
+      <Nav.Link href="/about" className="home-nav-link">
         <img className="home-btn" src={aboutButton} alt="about me" />
       </Nav.Link>
-      <Nav.Link to="skills" className="home-nav-link">
+      <Nav.Link href="/skills" className="home-nav-link">
         <img className="home-btn" src={skillsButton} alt="skills" />
       </Nav.Link>
-      <Nav.Link to="projects" className="home-nav-link">
+      <Nav.Link href="/projects" className="home-nav-link">
         <img className="home-btn" src={projectsButton} alt="projects" />
       </Nav.Link>
     </Nav>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,16 @@ import Nav from "react-bootstrap/Nav";
 function NavBar() {
   return (
     <Nav>
-      <Nav.Link className="nav-link" to="/">
+      <Nav.Link className="nav-link" href="/">
         Home
       </Nav.Link>
-      <Nav.Link className="nav-link" to="/about">
+      <Nav.Link className="nav-link" href="/about">
         About Me
       </Nav.Link>
-      <Nav.Link className="nav-link" to="/skills">
+      <Nav.Link className="nav-link" href="/skills">
         Skills
       </Nav.Link>
-      <Nav.Link className="nav-link" to="/projects">
+      <Nav.Link className="nav-link" href="/projects">
         Projects
       </Nav.Link>
     </Nav>
