0eec2b8629abc85e89ca7c85b48ab7fbf15c236b
Anri <anri0806@hotmail.co.jp>
2023-01-05 23:25:05 -0500

    added hover and show info feature on project

--- src/components/InviteOnly.js
+++ src/components/InviteOnly.js
@@ -1,17 +1,26 @@
+import inviteOnly from "../images/invite-only.png";
+
 function InviteOnly() {
   return (
     <div className="project-box">
-      <h4>Invite Only</h4>
-      <p>
-        A private social media app where invited users can share posts and
-        photos and create their own private group.
-      </p>
-      <a href="https://youtu.be/GQoX64CcZJA">
-        <p>demo</p>
-      </a>
-      <a href="https://github.com/anri0806/invite-only">
-        <p>code</p>
-      </a>
+      <div className="project-container">
+        <div className="default-image">
+          <img src={inviteOnly} alt="invite only" />
+        </div>
+        <div className="hover-info">
+          <h3>Invite Only</h3>
+          <p>
+            A private social media app where invited users can share posts and
+            photos and create their own private group.
+          </p>
+          <a href="https://youtu.be/GQoX64CcZJA">
+            <p>demo</p>
+          </a>
+          <a href="https://github.com/anri0806/invite-only">
+            <p>code</p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
