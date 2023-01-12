ef9ae255b6cb38204d1d8136b918e0d306b1ee3b
Anri <anri0806@hotmail.co.jp>
2023-01-11 23:09:51 -0500

    added new skill logo and modified content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,52 @@
 import NavBar from "./NavBar";
 
-import htmlLogo from "../images/html.png";
+import htmlLogo from "../images/html5.png";
 import cssLogo from "../images/css.png";
 import jsLogo from "../images/js.png";
 import reactLogo from "../images/react.png";
 import rubyLogo from "../images/ruby.png";
 import githubLogo from "../images/github-mark.png";
 import railsLogo from "../images/rails.png";
-import nodeLogo from "../images/nodeJS.png";
 import postgresLogo from "../images/PostgreSQL.png";
 import bootstrapLogo from "../images/bootstrap.png";
+import skillsTitle from "../images/skills-title.png";
 
 function Skills() {
   return (
     <div className="skills">
-      {/* <NavBar /> */}
-      <h1>Skills</h1>
-      <img className="skills-logo" src={htmlLogo} alt="html" />
-      <img className="skills-logo" src={cssLogo} alt="css" />
-      <img className="skills-logo" src={jsLogo} alt="js" />
-      <img className="skills-logo" src={reactLogo} alt="react" />
-      <img className="skills-logo" src={rubyLogo} alt="ruby" />
-      <img className="skills-logo" src={railsLogo} alt="rails" />
-      <img className="skills-logo" src={githubLogo} alt="github" />
-      <img className="skills-logo" src={nodeLogo} alt="nodeJS" />
-      <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" />
-      <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" />
+      s{/* <NavBar /> */}
+      <img className="skills-title" src={skillsTitle} alt="skills" />
+      <div className="skills-container">
+        <h4 className="skill">
+          <img className="skills-logo" src={htmlLogo} alt="html" /> HTML
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={cssLogo} alt="css" /> CSS
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={jsLogo} alt="js" /> Javascript
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={reactLogo} alt="react" /> React
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={rubyLogo} alt="ruby" /> Ruby
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={railsLogo} alt="rails" /> Rails
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={githubLogo} alt="github" /> GitHub
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" />
+          PostgreSQL
+        </h4>
+        <h4 className="skill">
+          <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" />
+          Bootstrap
+        </h4>
+      </div>
     </div>
   );
 }
