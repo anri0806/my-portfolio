f209be2d13f992ec13467c74b2e3a9356e8a33fc
Anri <anri0806@hotmail.co.jp>
2023-01-12 11:23:22 -0500

    added more styling to skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,53 +18,47 @@ function Skills() {
       <img className="skills-title" src={skillsTitle} alt="skills" />
       <div className="skills-container">
         <h4 className="skill-category">Front end</h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={htmlLogo} alt="html" />  */}
-          HTML
-        </h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={cssLogo} alt="css" />  */}
+        <p className="skill">
+          <img className="skills-logo" src={htmlLogo} alt="html" /> HTML
+        </p>
+        <p className="skill">
+          <img
+            style={{ width: "23px", padding: "0" }}
+            src={cssLogo}
+            alt="css"
+          />{" "}
           CSS
-        </h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={jsLogo} alt="js" />  */}
-          Javascript
-        </h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={reactLogo} alt="react" /> */}
-          React
-        </h4>
-        <h4 className="skill">Redux</h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" /> */}
+        </p>
+        <p className="skill">
+          <img className="skills-logo" src={jsLogo} alt="js" /> Javascript
+        </p>
+        <p className="skill">
+          <img className="skills-logo" src={reactLogo} alt="react" /> React
+        </p>
+        <p className="skill"> Redux</p>
+        <p className="skill">
+          <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" />{" "}
           Bootstrap
-        </h4>
+        </p>
         <h4 className="skill-category">Back end</h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={rubyLogo} alt="ruby" /> */}
-          Ruby
-        </h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={railsLogo} alt="rails" />  */}
-          Rails
-        </h4>
-        <h4 className="skill">Bcrypt</h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" /> */}
+        <p className="skill">
+          <img className="skills-logo" src={rubyLogo} alt="ruby" /> Ruby
+        </p>
+        <p className="skill">
+          <img style={{ width: "40px" }} src={railsLogo} alt="rails" /> Rails
+        </p>
+        <p className="skill">Bcrypt</p>
+        <p className="skill">
+          <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" />{" "}
           PostgreSQL
-        </h4>
-        <h4 className="skill">SQLite</h4>
+        </p>
+        <p className="skill">SQLite</p>
         <h4 className="skill-category">Others</h4>
-        <h4 className="skill">
-          {/* <img className="skills-logo" src={githubLogo} alt="github" />  */}{" "}
-          GitHub
-        </h4>
-        <h4 className="skill">
-          AWS
-        </h4>
-        <h4 className="skill">
-          Firebase
-        </h4>
+        <p className="skill">
+          <img className="skills-logo" src={githubLogo} alt="github" /> GitHub
+        </p>
+        <p className="skill"> AWS</p>
+        <p className="skill"> Firebase</p>
       </div>
     </div>
   );
