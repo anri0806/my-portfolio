0bd894a7a53f52a34538ea004d6b66535d22e237
Anri <anri0806@hotmail.co.jp>
2023-01-12 15:23:31 -0500

    added about me title img

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,8 @@
+import Carousel from 'react-bootstrap/Carousel';
+
 import NavBar from "./NavBar";
 
+import aboutMeTitle from "../images/about-me-title.png";
 import travelLogo from "../images/travel-and-tourism.png";
 import hikingLogo from "../images/hiking.png";
 import gameLogo from "../images/game-console.png";
@@ -12,24 +15,21 @@ import linkedInLogo from "../images/LI-In-Bug.png";
 import mediumLogo from "../images/medium.png";
 
 function AboutMe() {
+  /// ADD fun facts? => born and raised in Japan, memorized all the country flags
+
   return (
     <div className="about-me">
-    <NavBar />
-      <br />
-      <h1>AboutMe</h1>
+      <NavBar />
+      <img className="about-me-title" src={aboutMeTitle} alt="about me" />
+
       <p>
-        Hello! My name is Anri and I am a Full Stack Software Developer with a
-        background in the Hospitality and Real Estate industry. My interest in
-        programming and technology began in the middle school. I created my very
-        first website using simple HTML and completely fell in love with coding.
-        Although I decided to pursue a different career, I continued to have
-        passion in programing. While working in the Hospitality and Real Estate
-        industry, I helped the company that I worked for to update their website
-        using a cloud-based website builder. Doing this brought me back to the
-        moment where I touched code for the first time. During pandemic I
-        finally decided to pursue my passion and enrolled in the Flatiron
-        School's Full Stack Web Development Program where I fell in love with
-        programming again.
+        Hello! My name is Anri Kawahara and I am a Full Stack Software Developer
+        with a background in the Hospitality and Real Estate industry. My
+        interest in programming and technology began in the middle school. I
+        created my very first website using simple HTML and completely fell in
+        love with coding. During pandemic I finally decided to pursue my passion
+        and enrolled in the Flatiron School's Full Stack Web Development Program
+        where I fell in love with programming again!
       </p>
       <p>
         I believe that the passion can be driving force to learn more and give
