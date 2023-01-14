013f23fe6ac5f698b9f8754a80f7bfcea1cd8d8a
Anri <anri0806@hotmail.co.jp>
2023-01-14 00:36:00 -0500

    modified css styling on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import HomeNavBar from "./HomeNavBar";
 
+import brushImg from "../images/home-brush.png";
+
 function Home() {
   return (
     <div className="home">
       <p id="home-my-name">Anri Kawahara</p>
       <HomeNavBar />
+      <img id="home-brush" src={brushImg} alt="brush" />
       <p id="home-job-title">Full Stack Software Engineer</p>
     </div>
   );
