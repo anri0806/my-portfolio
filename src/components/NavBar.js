83d7377447b53bf0c43cfa572ae21586cbd7469f
Anri <anri0806@hotmail.co.jp>
2023-01-18 00:32:39 -0500

    added showing navBar on scroll feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,49 @@
+import { useState, useEffect } from "react";
+
 import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
+  const [visible, setVisible] = useState(false);
+
+  // Show navBar based on scroll location
+
+  function controlNavbar() {
+    if (window.scrollY < 750) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  }
+
+  // Activate controlNavbar func
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    return () => window.removeEventListener("scroll", controlNavbar);
+  }, []);
+
   return (
-      <Nav id="nav-bar">
-        <Nav.Link className="nav-link" href="#about-me">
-          01. About Me
-        </Nav.Link>
-        <Nav.Link className="nav-link" href="#skills">
-          02. Skills
-        </Nav.Link>
-        <Nav.Link className="nav-link" href="#projects">
-          03. Projects
-        </Nav.Link>
-      </Nav>
+    <Nav className={visible ? "nav-bar-visible" : "nav-bar"}>
+      <Nav.Link
+        className={visible ? "nav-visible" : "nav-not-visible"}
+        href="#about-me"
+      >
+        01. About Me
+      </Nav.Link>
+      <Nav.Link
+        className={visible ? "nav-visible" : "nav-not-visible"}
+        href="#skills"
+      >
+        02. Skills
+      </Nav.Link>
+      <Nav.Link
+        className={visible ? "nav-visible" : "nav-not-visible"}
+        href="#projects"
+      >
+        03. Projects
+      </Nav.Link>
+    </Nav>
   );
 }
 
