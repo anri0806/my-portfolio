39c5508030b8672d8d10b8f2e26507135daaabef
Anri <anri0806@hotmail.co.jp>
2023-01-19 01:31:59 -0500

    modified styling on skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,67 +1,128 @@
-
 import htmlLogo from "../images/html5.png";
 import cssLogo from "../images/css.png";
 import jsLogo from "../images/js.png";
 import reactLogo from "../images/react.png";
 import rubyLogo from "../images/ruby.png";
-import githubLogo from "../images/github-mark.png";
+import githubLogo from "../images/github-mark-black.png";
 import railsLogo from "../images/rails.png";
 import postgresLogo from "../images/PostgreSQL.png";
 import bootstrapLogo from "../images/bootstrap.png";
 import firebaseLogo from "../images/firebase.png";
+import reduxLogo from "../images/redux.png";
+import sqliteLogo from "../images/sqlite.png";
+import awsLogo from "../images/aws.png";
 
 import brushImg3 from "../images/brush3.png";
 
 function Skills() {
   return (
     <section className="skills" id="skills">
-
       <div className="skills-container">
-        <p className="skill-category">Front end</p>
-        <p className="skill">
-          <img className="skills-logo" src={htmlLogo} alt="html" /> HTML
-        </p>
-        <p className="skill">
-          <img
-            style={{ width: "23px", padding: "0" }}
-            src={cssLogo}
-            alt="css"
-          />{" "}
-          CSS
-        </p>
-        <p className="skill">
-          <img className="skills-logo" src={jsLogo} alt="js" /> Javascript
-        </p>
-        <p className="skill">
-          <img className="skills-logo" src={reactLogo} alt="react" /> React
-        </p>
-        {/* <p className="skill"> Redux</p> */}
-        <p className="skill">
-          <img className="skills-logo" src={bootstrapLogo} alt="bootstrap" />{" "}
-          Bootstrap
-        </p>
-        <p className="skill-category">Back end</p>
-        <p className="skill">
-          <img className="skills-logo" src={rubyLogo} alt="ruby" /> Ruby
-        </p>
-        <p className="skill">
-          <img style={{ width: "40px" }} src={railsLogo} alt="rails" /> Rails
-        </p>
-        <p className="skill">Bcrypt</p>
-        <p className="skill">
-          <img className="skills-logo" src={postgresLogo} alt="PostgreSQL" />{" "}
-          PostgreSQL
-        </p>
-        <p className="skill">SQLite</p>
-        <p className="skill-category">Others</p>
-        <p className="skill">
-          <img className="skills-logo" src={githubLogo} alt="github" /> GitHub
-        </p>
-        <p className="skill"> AWS</p>
-        <p className="skill">
-          <img className="skills-logo" src={firebaseLogo} alt="firebase" />{" "}
-          Firebase
-        </p>
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#e0c7b7" }} className="skills-logo">
+            <img src={htmlLogo} alt="html" />
+            <p className="skill">HTML</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#b7cee0" }} className="skills-logo">
+            <img
+              style={{ width: "50px", height: "50px", margin: "15px" }}
+              src={cssLogo}
+              alt="css"
+            />
+            <p className="skill">CSS</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#f7f7e4" }} className="skills-logo">
+            <img src={jsLogo} alt="js" />
+            <p className="skill">Javascript</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#d2ecf7" }} className="skills-logo">
+            <img src={reactLogo} alt="react" />
+            <p className="skill">React</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#cdb3d7" }} className="skills-logo">
+            <img src={reduxLogo} alt="redux" />
+            <p className="skill"> Redux</p>
+          </div>
+        </div>
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#e2cada" }} className="skills-logo">
+            <img src={bootstrapLogo} alt="bootstrap" />
+            <p className="skill">Bootstrap</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#d3adad" }} className="skills-logo">
+            <img src={rubyLogo} alt="ruby" />
+            <p className="skill">Ruby</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#dbb0cc" }} className="skills-logo">
+            <img
+              style={{ width: "50px", height: "50px", margin: "15px" }}
+              src={railsLogo}
+              alt="rails"
+            />
+            <p className="skill">Rails</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#b0b9db" }} className="skills-logo">
+            <img
+              style={{ width: "50px", height: "50px", margin: "15px" }}
+              src={postgresLogo}
+              alt="PostgreSQL"
+            />
+            <p className="skill">PostgreSQL</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#b0cfdb" }} className="skills-logo">
+            <img src={sqliteLogo} alt="sqlite" />
+            <p className="skill">SQLite</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#cacfd2" }} className="skills-logo">
+            <img src={githubLogo} alt="github" />
+            <p className="skill">GitHub</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#d8c7b2" }} className="skills-logo">
+            <img
+              style={{ width: "60%", height: "50%", margin: "20px 15px" }}
+              src={awsLogo}
+              alt="aws"
+            />
+            <p className="skill"> AWS</p>
+          </div>
+        </div>
+
+        <div className="skills-logo-container">
+          <div style={{ backgroundColor: "#d8c9b2" }} className="skills-logo">
+            <img src={firebaseLogo} alt="firebase" />
+            <p className="skill">Firebase</p>
+          </div>
+        </div>
       </div>
 
       <div id="skills-title">
