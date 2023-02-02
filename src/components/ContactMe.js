45edf7804f195973419651124d16533f83da30db
Anri <anri0806@hotmail.co.jp>
2023-02-02 12:35:40 -0500

    updated css on contact page to be mobile responsive

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -6,24 +6,18 @@ export default function ContactMe() {
   return (
     <section className="contact-me" id="contact-me">
       <div id="contact-me-title">
-        <p style={{ fontSize: "4vw" }}>04. </p>
-        <p style={{ fontWeight: "900" }}>Get In</p>
-        <p>Touch</p>
+        <div id="title">
+          <p style={{ fontSize: "4vw" }}>04. </p>
+          <p style={{ fontWeight: "900" }}>Get In</p>
+          <p>Touch</p>
+        </div>
 
-        <p
-          style={{
-            fontSize: "1.1vw",
-            fontFamily: "Roboto",
-            width: "70%",
-            lineHeight: "19px",
-            marginTop: "40px",
-          }}
-        >
+        <p id="contact-me-message">
           Feel free to contact me with any questions. I will get back to you as
           soon as possible!
         </p>
         <a href="mailto:anri0806ny@gmail.com">
-          <Button variant="light">
+          <Button id="contact-button" variant="light">
             <span style={{ fontFamily: "Roboto" }}>Say Hello</span>
           </Button>
         </a>
